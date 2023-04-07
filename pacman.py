1534ccbd176bbd8cdd30e96a924ccd372b260681
akarnam <akarnam@umich.edu>
2023-04-07 12:28:31 -0400

    created the game and wall objects

--- pacman.py
+++ pacman.py
@@ -30,3 +30,41 @@ class RenderGame:
             self.screen.fill(black)
             self.handle_events()
         print("Game Over ...")
+
+    def add_game_object(self, obj):
+        self.game_objects.append(obj)
+
+    def add_wall_to_game(self, obj):
+        self.add_game_object(obj)
+        self.walls.append(obj)
+
+    def handle_events(self):
+        pass
+
+
+class GameObject:
+    def __init__(self, in_surface, x, y, input_size, input_color=(255,0,0), is_circle=False):
+        self.size = input_size
+        self.renderer: RenderGame = in_surface
+        self.surface = in_surface.screen
+        self.x = x
+        self.y = y
+        self.color = input_color
+        self.circle = is_circle
+        self.shape = pygame.Rect(x, y, input_size, input_size)
+
+    def draw(self):
+        if self.circle:
+            pygame.draw.circle(self.surface, self.color, self.x, self.y, self.size)
+        else:
+            rectangle = pygame.Rect(self.x, self.y, self.size, self.size)
+            pygame.draw.rect(self.surface, self.color, rectangle, border_radius=4)
+
+    def tick(self):
+        pass
+
+
+class WallObject(GameObject):
+    def __init__(self, input_surface, x, y, input_size, input_color=(0, 0, 255)):
+        super().__init__(input_surface, x * input_size, y * input_size, input_size, input_color)
+
